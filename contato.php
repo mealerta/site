760c502c48a6d544473821efd0a650b76ea84cad
Fernando Rocha <fernando.rocha@mealerta.com>
2016-07-25 11:31:59 -0300

    First commit for site

--- contato.php
+++ contato.php
@@ -1,41 +1,45 @@
-<?php 
-if (isset($_POST['name']) and isset($_POST['email']) and isset($_POST['message']))  {
-  $name = $_POST['name'];
-  $email = $_POST['email'];
-  $message = $_POST['message'];
-  
-  $email_to = "contato@mealerta.com";
-  $email_subject = "Contato Site";
+  1 <?php
+  2 if (isset($_POST['name']) and isset($_POST['email']) and isset($_POST['message']))  {
+  3 
+  4   require_once('PHPMailer/class.phpmailer.php');
+  5 
+  6   $mailer = new PHPMailer();
+  7   $mail->IsSMTP();
+  8   $mail->SMTPAuth   = true;
+  9   $mail->SMTPSecure = "tls";
+ 10   $mail->Host       = "email-smtp.us-east-1.amazonaws.com";
+ 11   $mail->Username   = "KIAIESZISNA2KRWH2FQ";
+ 12   $mail->Password   = "AuLlxKoddZHk59QZUXjcXepM9DzsnoDkmDN // aDZSe3j";
+ 13 
+ 14   $mail->SetFrom('mealerta@mealerta.com', 'mealerta.com'; //from (verified email address)
+ 15   $mail->Subject = "Contato Site"; //subject
+ 16 
+ 17   //message
+ 18   $body = "Nome: ".$_POST['name']"\n";
+ 19   $body .= "E-mail: ".$_POST['email']"\n";
+ 20   $body .= "Mensagem: ".$_POST['message']"\n";
+ 21   $body = eregi_replace("[]",'',$body);
+ 22   $mail->MsgHTML($body);
+ 23   //
+ 24 
+ 25   //recipient
+ 26   $mail->AddAddress("mealerta@mealerta.com", "mealerta.com");
+ 27 
+ 28   //Success
+ 29   if ($mail->Send()) {
+ 30     echo "Obrigado pelo seu contato. Entraremos em contato com você em breve.";
+ 31     //die; 
+ 32     sleep(3);
+ 33     $redirect = "http://www.mealerta.com";
+ 34     header("location:$redirect");
+ 35   }
+ 36 
+ 37   //Error
+ 38   if(!$mail->Send()) {
+ 39     echo "E-mail não enviado.\nErro: " . $mail->ErrorInfo;
+ 40   }
+ 41 } else {
+ 42   echo "Desculpe, há um problema com os dados informados.";
+ 43 }
+ 44 ?>
 
-    function died($error) {
-        // your error code can go here
-        $html .= "Nós pedimos desculpas, mas há erro(s) no seu envio. ";
-        $html .= "Veja os erros abaixo.<br /><br />";
-        $html .= $error."<br /><br />";
-        $html .= "Por favor, volte e ajuste os erros..<br /><br />";
-         echo $html;
-        die();
-    }
-
-    $email_message = "Mais detalhes abaixo.\n\n";
-
-    function clean_string($string) {
-      $bad = array("content-type","bcc:","to:","cc:","href");
-      return str_replace($bad,"",$string);
-    }
-
-    $email_message .= "Nome: ".clean_string($name)."\n";
-    $email_message .= "E-mail: ".clean_string($email)."\n";
-    $email_message .= "Mensagem: ".clean_string($message)."\n";
-
-    // create email headers
-    $headers = 'From: '.$email."\r\n".
-    'Reply-To: '.$email."\r\n" .
-    'X-Mailer: PHP/' . phpversion();
-    @mail($email_to, $email_subject, $email_message, $headers);  
-
-    echo  "Obrigado pelo seu contato. Entraremos em contato com você em breve.";
-}   else {
-  echo "Desculpe, há um problema com os dados informados.";
-}
-?>
